625b919fc7c1459186b6210c6acf8c2afb0100e6
Ivan <adamovskij00@mail.ru>
2022-04-29 13:46:21 +0300

    add ApolloProvider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
 import Tabs from './components/Tabs/Tabs'
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
-
 import theme from './components/theme'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+    uri: 'http://localhost:3005/graphql',
+    cache: new InMemoryCache(),
+})
 
 class App extends Component {
     render() {
         return (
-            <MuiThemeProvider theme={theme}>
-                <Tabs />
-            </MuiThemeProvider>
+            <ApolloProvider client={client}>
+                <MuiThemeProvider theme={theme}>
+                    <Tabs />
+                </MuiThemeProvider>
+            </ApolloProvider>
         )
     }
 }
