2cc632077490d71628ee97d7a880467bdd54f1bd
Ivan <adamovskij00@mail.ru>
2022-04-29 12:04:12 +0300

    add upd DirectorsDialog, Directors code style

--- src/components/Directors/Directors.jsx
+++ src/components/Directors/Directors.jsx
@@ -1,47 +1,53 @@
-import React from 'react';
+import React from 'react'
 
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab'
+import AddIcon from '@material-ui/icons/Add'
 
-import DirectorsTable from '../DirectorsTable/DirectorsTable';
-import DirectorsForm from '../DirectorsForm/DirectorsForm';
+import DirectorsTable from '../DirectorsTable/DirectorsTable'
+import DirectorsForm from '../DirectorsForm/DirectorsForm'
 
-import withHocs from './DirectorsHoc';
+import withHocs from './DirectorsHoc'
 
 class Directors extends React.Component {
-  state = {
-    open: false,
-    name: '',
-    age: 0,
-  }
+    state = {
+        open: false,
+        name: '',
+        age: 0,
+    }
 
-  handleClickOpen = (data) => {
-    this.setState({
-      open: true,
-      ...data,
-    });
-  };
+    handleClickOpen = (data) => {
+        this.setState({
+            open: true,
+            ...data,
+        })
+    }
 
-  handleClose = () => { this.setState({ name: '', age: 0, id: null, open: false }); };
+    handleClose = () => {
+        this.setState({ name: '', age: 0, id: null, open: false })
+    }
 
-  handleChange = name => ({ target }) => { this.setState({ [name]: target.value }); };
+    handleChange = name => ({ target }) => {
+        this.setState({ [name]: target.value })
+    }
 
-  render() {
-    const { name, age, id, open } = this.state;
-    const { classes } = this.props;
+    render() {
+        const { name, age, id, open } = this.state
+        const { classes } = this.props
 
-    return (
-      <>
-        <DirectorsForm handleChange={this.handleChange} selectedValue={{ name, age, id }} open={open} onClose={this.handleClose} />
-        <div className={classes.wrapper}>
-          <DirectorsTable onOpen={this.handleClickOpen} onClose={this.handleClose} />
-          <Fab onClick={() => this.handleClickOpen(null)} color="primary" aria-label="Add" className={classes.fab}>
-            <AddIcon />
-          </Fab>
-        </div>
-      </>
-    );
-  }
-};
+        return (
+            <>
+                <DirectorsForm handleChange={this.handleChange} selectedValue={{ name, age, id }} open={open}
+                               onClose={this.handleClose} />
+                <div className={classes.wrapper}>
+                    <DirectorsTable onOpen={this.handleClickOpen} onClose={this.handleClose} />
+                    <Fab onClick={() => this.handleClickOpen(null)} color='primary' aria-label='Add'
+                         className={classes.fab}>
+                        <AddIcon />
+                    </Fab>
+                </div>
+            </>
+        )
+    }
+}
 
-export default withHocs(Directors);
+export default withHocs(Directors)
