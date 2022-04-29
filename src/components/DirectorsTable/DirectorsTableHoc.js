bc82387ca6884d3534c342c9b08d293ccb3c9e5c
Ivan <adamovskij00@mail.ru>
2022-04-29 15:45:42 +0300

    add directorsQuery to compose

--- src/components/DirectorsTable/DirectorsTableHoc.js
+++ src/components/DirectorsTable/DirectorsTableHoc.js
@@ -1,6 +1,7 @@
 import { withStyles } from '@material-ui/core/styles'
 import { compose } from 'recompose'
-
 import { styles } from './styles'
+import { graphql } from 'react-apollo'
+import { directorsQuery } from './queries'
 
-export default compose(withStyles(styles))
+export default compose(withStyles(styles), graphql(directorsQuery))
