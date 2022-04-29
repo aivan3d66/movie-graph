eeab15a36dfb2468bd331537d1dbb6bea05eacaa
Ivan <adamovskij00@mail.ru>
2022-04-29 18:26:19 +0300

    add addMovieMutation to withGraphqlAdd compose

--- src/components/MoviesForm/MoviesFormHoc.js
+++ src/components/MoviesForm/MoviesFormHoc.js
@@ -1,6 +1,18 @@
 import { withStyles } from '@material-ui/core/styles'
 import { compose } from 'recompose'
-
 import { styles } from './styles'
+import { graphql } from 'react-apollo'
+import { addMovieMutation } from './mutation'
+import { moviesQuery } from '../MoviesTable/queries'
+import { directorsQuery } from './queries'
 
-export default compose(withStyles(styles))
+const withGraphqlAdd = graphql(addMovieMutation, {
+    props: ({ mutate }) => ({
+        addMovie: movie => mutate({
+            variables: movie,
+            refetchQueries: [{ query: moviesQuery }],
+        }),
+    }),
+});
+
+export default compose(withStyles(styles), withGraphqlAdd, graphql(directorsQuery));
