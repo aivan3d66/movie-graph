d64e3d2d4b2c491793133f2ece7e5a75108bd2e8
Ivan <adamovskij00@mail.ru>
2022-04-29 18:31:49 +0300

    add addDirectorMutation to withGraphqlAdd compose

--- src/components/DirectorsForm/DirectorsFormHoc.js
+++ src/components/DirectorsForm/DirectorsFormHoc.js
@@ -1,6 +1,17 @@
 import { withStyles } from '@material-ui/core/styles'
 import { compose } from 'recompose'
-
 import { styles } from './styles'
+import { graphql } from 'react-apollo'
+import { addDirectorMutation } from './mutation'
+import { directorsQuery } from '../DirectorsTable/queries'
 
-export default compose(withStyles(styles))
+const withGraphqlAdd = graphql(addDirectorMutation, {
+    props: ({ mutate }) => ({
+        addDirector: director => mutate({
+            variables: director,
+            refetchQueries: [{ query: directorsQuery }],
+        }),
+    }),
+})
+
+export default compose(withStyles(styles), withGraphqlAdd)
