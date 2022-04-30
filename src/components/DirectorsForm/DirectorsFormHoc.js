2be653f47078082506f7b842933513a087207f0e
Ivan <adamovskij00@mail.ru>
2022-04-30 20:41:28 +0300

    add withGraphQL HOC with addDirectorMutation and updatedDirectorMutation

--- src/components/DirectorsForm/DirectorsFormHoc.js
+++ src/components/DirectorsForm/DirectorsFormHoc.js
@@ -2,16 +2,26 @@ import { withStyles } from '@material-ui/core/styles'
 import { compose } from 'recompose'
 import { styles } from './styles'
 import { graphql } from 'react-apollo'
-import { addDirectorMutation } from './mutation'
+import { addDirectorMutation, updatedDirectorMutation } from './mutation'
 import { directorsQuery } from '../DirectorsTable/queries'
 
-const withGraphqlAdd = graphql(addDirectorMutation, {
-    props: ({ mutate }) => ({
-        addDirector: director => mutate({
-            variables: director,
-            refetchQueries: [{ query: directorsQuery }],
+const withGraphQL = compose(
+    graphql(addDirectorMutation, {
+        props: ({ mutate }) => ({
+            addDirector: director => mutate({
+                variables: director,
+                refetchQueries: [{ query: directorsQuery }],
+            }),
         }),
     }),
-})
+    graphql(updatedDirectorMutation, {
+        props: ({ mutate }) => ({
+            updateDirector: director => mutate({
+                variables: director,
+                refetchQueries: [{ query: directorsQuery }],
+            }),
+        }),
+    })
+);
 
-export default compose(withStyles(styles), withGraphqlAdd)
+export default compose(withStyles(styles), withGraphQL);
